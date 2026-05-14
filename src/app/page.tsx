9d26992914dd65dca02e2f402287832cc13d4dbd
PredioTop <tcc.prediotop@gmail.com>
2026-05-14 15:19:18 -0300

    tentando resolver

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { redirect } from "next/navigation";
 
-<<<<<<< HEAD
-// Teste de commit
-=======
->>>>>>> 669a38eb7a425d21cd0472161b538b342eceaeca
 import { Role } from "@/lib/prisma";
 
 import { resolveSession } from "@/lib/auth/session";
